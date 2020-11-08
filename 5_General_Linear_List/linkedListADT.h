edfa32ac286b416168ce00dd10ea01a0f0c61c5d
tsg0019 <tsg0019@naver.com>
2020-11-09 02:50:09 +0900

    Perfectly_working_General_linear_list

--- 5_General_Linear_List/linkedListADT.h
+++ 5_General_Linear_List/linkedListADT.h
@@ -3,11 +3,14 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-typedef struct node{
+ 
+
+typedef struct node {
     void* dataptr;
     struct node* link;
 }   NODE;
-typedef struct{
+
+typedef struct {
     int count;
     NODE* pos;
     NODE* head;
@@ -15,24 +18,30 @@ typedef struct{
     int (*compare) (void* argu1, void* argu2);   //compare function pointer
 } LIST;
 
-LIST* createList (int (*compare)(void* argu1, void* argu2));
-LIST* destroyList (LIST* list);
-int addNode (LIST* pList, void* dataInPtr);
+ 
+
+LIST* createList(int (*compare)(void* argu1, void* argu2));
+LIST* destroyList(LIST* list);
+int addNode(LIST* pList, void* dataInPtr);
 bool removeNode(LIST* pList, void* pArgu, void** pDataOut);
+bool traverse(LIST* pList, int fromWhere, void** dataOutPtr);
 bool retrieveNode(LIST* pList, void* pArgu, void** dataOutPtr);
-bool traverse (LIST* pList, int fromWhere, void** dataOutPtr);
-int listCount (LIST* pList);
+int listCount(LIST* pList);
 bool emptyList(LIST* pList);
 bool fullList(LIST* pList);
+
+ 
 
 static bool _insert(LIST* pList, NODE* pPre, void* dataInPtr);
 static void _delete(LIST* pList, NODE* pPre, NODE* pLoc, void** dataOutPtr);
 static bool _search(LIST* pList, NODE** pPre, NODE** pLoc, void* pArgu);
 
-LIST* createList (int (*compare)(void* argu1, void* argu2)){
+ 
+
+LIST* createList(int (*compare)(void* argu1, void* argu2)) {
     LIST* list;
     list = (LIST*)malloc(sizeof(LIST));
-    if(list){
+    if (list) {
         list->head = NULL;
         list->pos = NULL;
         list->rear = NULL;
@@ -41,165 +50,175 @@ LIST* createList (int (*compare)(void* argu1, void* argu2)){
     }
     return list;
 }
-int addNode(LIST* pList, void* dataInPtr){
-    bool found;
+int addNode(LIST* pList, void* dataInPtr) {
+    bool found;//성공하면 0, 오버플로우 발생은 -1, 같은 값 존재하면 1 반환
     bool success;
-    NODE* pPre;
+    NODE* pPre ;
     NODE* pLoc;
-    found = _search(pList, &pPre,&pLoc, dataInPtr);
-    if(found){
-        return +1;
+    found = _search(pList, &pPre, &pLoc, dataInPtr);
+    if (found) {
+        return (+1);//똑같은 값을 찾은 경우 이므로 값을 넣어줄 필요 없어서 종료
+    }   //나머지 결과가 false인 경우에 대해서는 모두 삽입연산을 수행
+    success = _insert(pList, pPre, dataInPtr);
+    if (!success) {//success == false 일 때 실행
+        return (-1);   //삽입에 실패한 경우
     }
-    success = _insert(pList,pPre, dataInPtr);
-    if(!success){
-        return-1;
-    }
-    return 0;
+    return (0);//정상처리 된 경우 +1
 }
-static bool _insert(LIST* pList, NODE* pPre, void* dataInPtr){
+
+static bool _search(LIST* pList, NODE** pPre, NODE** pLoc, void* pArgu) {
+#define COMPARE ( ((* pList->compare) (pArgu, (*pLoc)->dataptr)) )
+#define COMPARE_LAST  ((* pList->compare) (pArgu, pList->rear->dataptr))
+    int result;
+    *pPre = NULL;
+    *pLoc = pList->head;
+    if (pList->count == 0) {
+        return false;   //list가 비어있는 경우 fals
+    }
+    else if(COMPARE < 0){   //내가 추가한 case
+        *pPre = NULL;
+        *pLoc = pList->head;
+        return false;
+    }
+
+    else if (COMPARE_LAST > 0) {
+        *pPre = pList->rear;
+        *pLoc = NULL;
+        return false;   //최대값 보다 큰 경우 false
+    }
+    while ((result = COMPARE) > 0) {
+        *pPre = *pLoc;
+        *pLoc = (*pLoc)->link;  //  하나씩 옮겨가면서 비교
+    }
+    if (result == 0) {
+        return true;    //  똑같은 값을 찾았을 경우 true
+    }
+    else {//이때는 비교함수의 return값이 -1이라는 거니깐 
+        return false;   //결국 똑같은 값을 찾지 못했다면 false
+    }                      //그리고 pPre,pLoc에는 넣을 자리가 저장돼 있을건데.. 
+}
+
+static bool _insert(LIST* pList, NODE* pPre, void* dataInPtr) {
     NODE* pNew;
-    if(!(pNew = (NODE*)malloc(sizeof(NODE)))){
-        return false ; 
+    if (!(pNew = (NODE*)malloc(sizeof(NODE)))) {
+        return false;       //자리가 없는 경우 아니면 다 삽입하여 true반환
     }
     pNew->dataptr = dataInPtr;
     pNew->link = NULL;
-    if(pPre = NULL){
+    if (pPre == NULL) { //젤 앞에 들어가야 하는 경우
         pNew->link = pList->head;
         pList->head = pNew;
-        if(pList->count == 0){
+        if (pList->count == 0) {
             pList->rear = pNew;
         }
     }
     else{
         pNew->link = pPre->link;
         pPre->link = pNew;
-        if(pNew->link == NULL){
+        if (pNew->link == NULL) {
             pList->rear = pNew;
         }
     }
     (pList->count)++;
     return true;
 }
-bool removeNode(LIST* pList, void* keyPtr, void** dataOutPtr){
-    bool found;
 
+bool removeNode(LIST* pList, void* keyPtr, void** dataOutPtr) {
+    bool found;
     NODE* pPre;
     NODE* pLoc;
-
     found = _search(pList, &pPre, &pLoc, keyPtr);
-    if(found){
+    if (found) {
         _delete(pList, pPre, pLoc, dataOutPtr);
     }
     return found;
 }
-static void _delete(LIST* pList, NODE* pPre, NODE* pLoc, void** dataOutPtr){
+
+static void _delete(LIST* pList, NODE* pPre, NODE* pLoc, void** dataOutPtr) {
     *dataOutPtr = pLoc->dataptr;
-    if(pPre == NULL){
+    if (pPre == NULL) {
         pList->head = pLoc->link;
     }
-    else{
+    else {
         pPre->link = pLoc->link;
     }
-    if(pLoc->link = NULL){
+    if (pLoc->link == NULL) {
         pList->rear = pPre;
     }
     (pList->count)--;
     free(pLoc);
     return;
 }
-bool searchList(LIST* pList, void* pArgu, void** pDataOut){
-    bool found;
 
+bool searchList(LIST* pList, void* pArgu, void** pDataOut) {
+    bool found;
     NODE* pPre;
     NODE* pLoc;
-
     found = _search(pList, &pPre, &pLoc, pArgu);
-    if(found){
+    if (found) {
         *pDataOut = pLoc->dataptr;
     }
-    else{
+    else {
         *pDataOut = NULL;
     }
     return found;
 }
-static bool _search(LIST* pList, NODE** pPre, NODE** pLoc, void* pArgu){
-    #define COMPARE ( ((* pList->compare) (pArgu, (*pLoc)->dataptr)) )
-    #define COMPARE_LAST  ((* pList->compare) (pArgu, pList->rear->dataptr))
-    int result;
-    *pPre = NULL;
-    *pLoc = pList->head;
-    if(pList->count == 0){
-        return false;
-    }
-    if(COMPARE_LAST > 0){
-        *pPre = pList->rear;
-        *pLoc = NULL;
-        return false;
-    }
-    while((result = COMPARE) > 0){
-        *pPre = *pLoc;
-        *pLoc = (*pLoc)->link;
-    }
-    if(result == 0){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-bool retrieveNode(LIST* pList, void* pArgu, void** dataOutPtr){
+
+bool retrieveNode(LIST* pList, void* pArgu, void** dataOutPtr) {
     bool found;
     NODE* pPre;
     NODE* pLoc;
     found = _search(pList, &pPre, &pLoc, pArgu);
-    if(found){
+    if (found) {
         *dataOutPtr = pLoc->dataptr;
         return true;
     }
     *dataOutPtr = NULL;
     return false;
 }
-bool emptyList(LIST* pList){
+
+bool emptyList(LIST* pList) {
     return (pList->count == 0);
 }
-int listCount(LIST* pList){
+int listCount(LIST* pList) {
     return pList->count;
 }
-bool fullList(LIST* pList){
+bool fullList(LIST* pList) {
     NODE* temp;
-    if((temp = (NODE*)malloc(sizeof(*(pList->head))))){
+    if ((temp = (NODE*)malloc(sizeof(*(pList->head))))) {
         free(temp);
         return false;
     }
     return true;
 }
-bool traverse(LIST* pList, int fromWhere, void**dataPtrOut){
-    if(pList->count == 0){
+
+bool traverse(LIST* pList, int fromWhere, void** dataPtrOut) {
+    if (pList->count == 0) {
         return false;
     }
-    if(fromWhere == 0){
+    if (fromWhere == 0) {
         pList->pos = pList->head;
         *dataPtrOut = pList->pos->dataptr;
         return true;
     }
-    else{
-        if(pList->pos->link == NULL){
+    else {
+        if (pList->pos->link == NULL) {
             return false;
         }
-        else{
+        else {
             pList->pos = pList->pos->link;
             *dataPtrOut = pList->pos->dataptr;
             return true;
         }
     }
 }
-LIST* destroyList(LIST* pList){
+LIST* destroyList(LIST* pList) {
     NODE* deletePtr;
-    if(pList){
-        while(pList->count > 0){
+    if (pList) {
+        while (pList->count > 0) {
             free(pList->head->dataptr);
             deletePtr = pList->head;
-            pList->head = pList->head->link;
+            deletePtr = deletePtr->link;
             pList->count--;
             free(deletePtr);
         }
--- 5_General_Linear_List/main.c
+++ 5_General_Linear_List/main.c
@@ -4,11 +4,6 @@
 #include <string.h>
 #include <ctype.h>
 #include "linkedListADT.h"
-/*- 노드가 가리키는 데이터는 양의 정수인 키(Key) 값으로만 구성된다고 가정
-- 테스트 절차를 수행하는 main 함수는 자유롭게 작성하면 됨
-- 추가로 탐색(search)도 테스트해보면 좋겠음 (리스트 내에 target 데이터가 있는 경우와 없는 경우)
-- 정상적인 동작 확인을 위해, 각 테스트 로직을 수행한 후 리스트 내의 전체 데이터들을 순회(traverse)하면서 화면에 출력해 보면 됨 (이를 화면 캡쳐하여 제출)
-- ADT 라이브러리의 활용은 PPT에서 설명한 Academy Awards 예를 참조*/
 const short STR_MAX = 41;
 typedef struct {
     short number;
@@ -17,151 +12,308 @@ void printInstr(void);
 LIST* buildList(void);
 void process(LIST* list);
 char getChoice(void);
-void printList (LIST* list);
-void search (LIST* list);
+void printList(LIST* list);
+void search(LIST* list);
+int cmpint(void* integer1, void* integer2);
 
-int cmpint(void* integer1,void* integer2);
-
-int main(void){
-    LIST* list;
+int main(void) {
+    LIST* list = NULL;
     printInstr();
+    short newdata;    int status;
+    list = createList(list);
+    newdata = 10;
+    printf("\n///insert a node before the first data node\n");
+    addNode(list, &newdata);
+    printList(list);
     list = buildList();
-    //process(list);
+    int newdata2 = 1;    
+    status = addNode(list, &newdata2);    
+    printf("\n//////// case : insert a node before the first data node");
+    printList(list);
+    int newdata1 = 11;
+    status = addNode(list, &newdata1);    
+    printf("\n////////case : insert a node after the last nodes");
+    printList(list);
+    int newdata3 = 8;
+    status = addNode(list, &newdata3);    
+    printf("\n////////case : insert a node between data nodes");
+    printList(list);
+     process(list);
     printf("\nEveryting done\n");
-    printf("**TEST\n");
     return 0;
 }
 
-void printInstr(void){
+void printInstr(void) {
     printf("지금부터 linkedListADT.h의 테스트를 진행하겠습니다.\n");
 }
 
-LIST* buildList(void){
+LIST* buildList(void) {
     FILE* fpData;
     LIST* list;
-
     short numIn;
+
     int addResult;
 
+ 
+
     PICTURE* pPic;
+
     list = createList(cmpint);
-    if(!list){
+
+    if (!list) {
+
         printf("\a Cannot create list\n"), exit(100);
+
     }
-    fpData = fopen("numbers.dat","r");
-    
-    printf("\n무사히 buildList 진입\n");//여기까지 문제 없는 것 확인.
-    printf("\n현재 파일의 위치 : %p\n",fpData);
-    if(!fpData){  //문제발생 예측되는 지역
+
+    fpData = fopen("numbers.dat", "r");
+
+ 
+
+    printf("\n=======numbers.dat에 있는 data를 link에 추가======");
+
+    if (!fpData) {  
+
         printf("\aError opening input file\n"), exit(100);
+
     }
-    while(fscanf(fpData,"%hd",&numIn) == 1){
-        printf("\nnow Inside the loop\n");//여기까지 문제 없는 것 확인.
+
+    while (fscanf(fpData, "%hd", &numIn) == 1) {
+
         pPic = (PICTURE*)malloc(sizeof(PICTURE));
-        if(!(pPic)){
+
+        if (!(pPic)) {
+
             printf("\aOut of Memory in build list\n"), exit(100);
+
         }
+
         pPic->number = numIn;
-        printf("\nnumIn : %d \n\n\n",numIn);
-        while((fgetc(fpData)) != '\t'){//문제발생지역!!!!!!!무한루프!!!!!!!!!
-            printf("\n**TEST**\n");
+
+        printf("\n현재 리스트에 추가 될 numIn : %d \n", numIn);
+
+        while ((fgetc(fpData)) != '\t') {
+
             ;
+
         }
-        addResult = addNode(list,pPic);
+
+        addResult = addNode(list, pPic);
+
         printList(list);
-        printf("\n**TEST**\n");
-        if(addResult != 0){
-            if(addResult == -1){
-                printf("Memory oerflow adding\a\n"),exit(120);
-            }else{
-                printf("Duplicate year %hd not added\n\a",pPic->number);
-                while(fgetc(fpData) != '\n')
-                    ;
+
+        
+
+        if (addResult != 0) {
+
+            if (addResult == -1) {
+
+                printf("NOTE!! : 메모리 overflow 발생\a\n"), exit(120);
+
             }
+
+            else {
+
+                printf("이미 key 값 %hd를 가지는 노드가 존재합니다. 추가하지 않았습니다.\n\a", pPic->number);
+
+                printf("\n=======numbers.dat에 있는 data를 link에 추가끝!!!======\n");
+
+                return list;
+
+            }
+
         }
+
     }
-    printf("\n무사히 buildList 빠져나가는지 확인\n");//여기까지 문제 없는 것 확인.
+
+    printf("=======numbers.dat에 있는 data를 link에 추가끝!!!======\n");
+
     return list;
+
 }
-void process(LIST* list){
+
+void process(LIST* list) {
+
     char choice;
-    do{
+
+    do {
+
+        printf("\n**TEST\n");
+
         choice = getChoice();
-        switch(choice){
-            case 'P' : printList(list);
+
+        switch (choice) {
+
+        case 'P': printList(list);
+
             break;
-            case 'S' : search(list);
-            case 'Q' : break;
+
+        case 'S': search(list);
+
+        case 'Q': break;
+
         }
-    }while(choice != 'Q');
+
+    } while (choice != 'Q');
+
     return;
+
 }
-char getChoice(void){
+
+char getChoice(void) {
+
     char choice;
+
     bool valid;
+
     printf("======== MENU =======\n"
-    "Here are your choices : \n"
-    "   S : Search for a number\n"
-    "   P : Print all numbers\n"
-    "   Q : Quit            \n\n"
-    "Enter your choice: ");
-    do{
-        scanf("%c",&choice);
+
+        "Here are your choices : \n"
+
+        "   S : Search for a number\n"
+
+        "   P : Print all numbers\n"
+
+        "   Q : Quit            \n\n"
+
+        "Enter your choice: ");
+
+    do {
+
+        scanf("%c", &choice);
+
         choice = toupper(choice);
-        switch(choice){
-            case 'S' : 
-            case 'P' :
-            case 'Q' : valid = true;break;
-            default : valid = false;
-            printf("\a\n Invalid choice \n Please try again\n");
-            break;
+
+        while (choice == '\n') { 
+
+            choice = getchar();
+
         }
-    }while(!valid);
+
+        printf("\nChoice는 현재 %c\n",choice);
+
+        switch (choice) {
+
+        case 'S':
+
+        case 'P':
+
+        case 'Q':   valid = true;
+
+                    break;
+
+        /*case '\n':  getchar();
+
+            valid = false; break;*/
+
+        default:    valid = false;
+
+                    printf("\a\n Invalid choice Please try again\n");
+
+                    break;
+
+        }
+
+    } while (!valid);
+
     return choice;
+
 }
-void printList (LIST* list){
+
+void printList(LIST* list) {
+
     PICTURE* pPic;
-    if(listCount(list) == 0){
+
+    if (listCount(list) == 0) {
+
         printf("현재 빈 리스트 입니다.\n");
+
     }
-    else{
-        printf("\n Best Pictures List\n");
-        traverse(list,0,(void**)&pPic);
-        do{
-            printf("%hd\n",pPic->number);
-        }while(traverse(list,1,(void**)&pPic));
+
+    else {
+
+        printf("\n-------------현재 List\n");
+
+        traverse(list, 0, (void**)&pPic);
+
+        do {
+
+            printf("%hd ", pPic->number);
+
+        } while (traverse(list, 1, (void**)&pPic));
+
     }
-    printf("Complete to Print All List\n");
+
+    printf("\n-------------현재 List 출력완료\n\n");
+
 }
-void search(LIST* list){
+
+void search(LIST* list) {
+
     short number;
+
     bool found;
+
     PICTURE pSrchArgu;
+
     PICTURE* pPic;
+
     printf("Enter number : ");
+
     scanf("%hd", &number);
+
     pSrchArgu.number = number;
-    found = searchList(list,&pSrchArgu,(void**)&pPic);
-    if(found){
-        printf("%hd\n", pPic->number);
-    }else{
-        printf("Sorry, but %d is not abailable.\n",number);
+
+    found = searchList(list, &pSrchArgu, (void**)&pPic);
+
+    if (found) {
+
+        printf("%hd는 현재 리스트 내에 존재한답니다.\n", pPic->number);
+
     }
+
+    else {
+
+        printf("죄송합니다. %d 는 존재하지 않습니다..\n", number);
+
+    }
+
     return;
+
 }
-int cmpint(void* integer1,void* integer2){
+
+int cmpint(void* integer1, void* integer2) {
+
     int result;
+
     short num1;
+
     short num2;
+
     num1 = ((PICTURE*)integer1)->number;
+
     num2 = ((PICTURE*)integer2)->number;
 
-    if(num1 < num2){
+ 
+
+    if (num1 < num2) {
+
         result = -1;
-    }else if(num1 > num2){
-        result = +1;
-    }else{
-        result = 0;
+
     }
+
+    else if (num1 > num2) {
+
+        result = +1;
+
+    }
+
+    else {
+
+        result = 0;
+
+    }
+
     return result;
+
 }
